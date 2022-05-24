9711c3874ddb54accba6c54dbb1b64259382d693
mihailot01 <mihailo.t.001@gmail.com>
2022-05-24 21:33:41 +0200

    cors enabled

--- routes/recepti.js
+++ routes/recepti.js
@@ -19,6 +19,7 @@ router.get('/', prikazi);
 router.get('/moji', auth, mojiRecepti);
 router.get('/omiljeni', auth, omiljeniRecepti);
 router.get('/pretraga', pretraga);
+router.post('/pretraga', pretraga);
 router.get('/:id_recepta', prikazi1);
 router.post('/', auth, noviRecept);
 router.post('/:id_recepta/like', auth, staviLajk);
